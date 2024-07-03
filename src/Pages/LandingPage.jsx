80751674949624edc9a4d63e4ea84ab6380794f3
Pedro <125074965+PedroMaxis@users.noreply.github.com>
2024-07-03 11:47:56 -0300

    Add files via upload

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,19 +1,38 @@
 import { Header } from "../Components/Header"
 import { Card } from "../Components/Card"
+import { useState, useEffect } from 'react';
+
+
 export function LandingPage() {
+    const [ teams, setTeams ] = useState([])
+
+useEffect(() => {
+    const buscarTeams = async () => {
+        const response = await fetch('./teams.json')
+        const data = await response.json()
+        setTeams(data.times_nba)
+        console.log(data.times_nba)
+    }
+    buscarTeams()
+   
+}, []) 
+
+  
     return(
         <>
-            <Header></Header>
-            <div className="mt-3 ml-3 mr-3 shrink-0 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 justify-center">
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-           
+            <Header/>
+            <div className=" mt-3 ml-3 mr-3 shrink-0 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 justify-center">
+
+            {
+                teams.map((team) =>
+                <Card 
+                id={team.id}
+                nome={team.nome}
+                logo_url={team.logo_url}/>
+               
+                )      
+            } 
+            
             </div>
         </>
 
