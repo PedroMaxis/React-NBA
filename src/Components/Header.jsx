0e239d0b7a5c718cc1dc26eae21d4f9e5c345b0b
PedroMaxis <pedro11sales14@gmail.com>
2024-08-05 10:31:01 -0300

    feat: correct bugs

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ export function Header() {
         <>
 <div className="bg-[#0e0384] pt-[5px] pr-9 pb-[5px] pl-9 flex flex-row gap-[931px] items-center justify-start self-stretch shrink-0 relative overflow-hidden">
       <a href="/" className="text-[#37ffdb] text-left font-mono text-[32px] font-normal relative">
-        NBA Log{" "}
+        NBA <span><img src="https://cdn-icons-png.flaticon.com/512/2527/2527948.png" className="w-7 h-7 -m-3 -mt-4 inline-block" alt="NBA" /></span>
       </a>
     </div>
 
