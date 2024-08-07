54c1947a09b307c150771a70736921346e7d8c67
PedroMaxis <pedro11sales14@gmail.com>
2024-08-07 10:29:53 -0300

    feat: change background

--- src/Components/cardJogadores.jsx
+++ src/Components/cardJogadores.jsx
@@ -1,7 +1,7 @@
 export function CardJogadores({nome,image_url}){
     return(
         <>
-        <div className="rounded-xl bg-[#ca1010] pt-4 pr-9 pb-[30px] pl-9 flex gap-2.5 items-end justify-center flex-wrap relative overflow-hidden">
+        <div className="rounded-xl bg-[#50c878] pt-4 pr-9 pb-[30px] pl-9 flex gap-2.5 items-end justify-center flex-wrap relative overflow-hidden">
     <h1 className="text-xl">{nome}</h1>
 <img className="min-w-44 md:h-[250px] lg:h-[700px] rounded-xl object-cover" src={image_url} alt={nome} />
 </div>
